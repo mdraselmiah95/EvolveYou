4197fd0e9d1830827b2c5c2eda653795cea59fed
mdraselmiah95 <russelroy15@gmail.com>
2023-03-06 00:21:25 +0600

    WIP on Evolve You Navbar

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import "./App.css";
 import Home from "./pages/Home";
 import ExerciseDetails from "./pages/ExerciseDetails";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: "1488px" } }}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetails />} />
       </Routes>
+      <Footer />
     </Box>
   );
 }
