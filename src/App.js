6ad3cfb7a90495fc0d3a2fdfcfe15a0dc3be29ad
mdraselmiah95 <russelroy15@gmail.com>
2023-03-06 00:08:59 +0600

    File setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import Home from "./pages/Home";
+import ExerciseDetails from "./pages/ExerciseDetails";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <>
-      <h2>This is gone.</h2>
-    </>
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+      </Routes>
+    </Box>
   );
 }
 
