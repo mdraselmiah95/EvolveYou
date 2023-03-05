589404bc14312666024594df503e46fc59c65a5f
mdraselmiah95 <russelroy15@gmail.com>
2023-03-06 00:30:15 +0600

    WIP on Evolve You Banner

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,16 @@ import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
   return (
-    <Stack>
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: "123px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        justifyContent: "none",
+      }}
+      px="20px"
+    >
       <Link to="/">
         <img
           src={Logo}
