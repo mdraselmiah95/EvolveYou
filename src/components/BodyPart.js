73d8d8908f53e2075bfb6288c06fa05d17fddbd5
mdraselmiah95 <russelroy15@gmail.com>
2023-03-06 02:50:41 +0600

    WIP on Evolve You Body Part

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -30,10 +30,6 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
               gap: "47px",
             }
       }
-      //   onClick={() => {
-      //     setBodyPart(item);
-      //     window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
-      //   }}
     >
       <img
         src={Icon}
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,7 +7,8 @@ export const exerciseOption = {
 };
 
 export const fetchData = async (url, options) => {
-  const response = await fetch(url, options);
-  const data = await response.json();
+  const res = await fetch(url, options);
+  const data = await res.json();
+
   return data;
 };
