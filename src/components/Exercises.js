7f9d75fe109d176b5982b5f6965ced5942f45200
mdraselmiah95 <russelroy15@gmail.com>
2023-03-07 01:46:17 +0600

    Show Exercises

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Exercises = () => {
-  return <div>Exercises</div>;
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  console.log(exercises);
+  return (
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack></Stack>
+    </Box>
+  );
 };
 
 export default Exercises;
