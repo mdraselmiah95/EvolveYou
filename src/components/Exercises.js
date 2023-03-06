cd99105d369f3a092912ee6336de54b5f3a5577e
mdraselmiah95 <russelroy15@gmail.com>
2023-03-07 02:58:29 +0600

    Show Exercises

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,25 +2,78 @@ import React, { useEffect, useState } from "react";
 import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 import ExerciseCard from "./ExerciseCard";
+import { exerciseOption, fetchData } from "../utils/fetchData";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
-  console.log(exercises);
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisePerPage = 9;
+
+  useEffect(() => {
+    const baseURL = "https://exercisedb.p.rapidapi.com/exercises";
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(baseURL, exerciseOption);
+      } else {
+        exercisesData = await fetchData(
+          `${baseURL}/bodyPart/${bodyPart}`,
+          exerciseOption
+        );
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart, setExercises]);
+
+  // Pagination
+  const indexOfLastExercise = currentPage * exercisePerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (event, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
   return (
-    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
-      <Typography variant="h3" mb="46px">
+    <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="46px"
+      >
         Showing Results
       </Typography>
       <Stack
         direction="row"
-        sx={{ gap: { lg: "110px", sx: "50px" } }}
-        flexWrap="wrap
-      "
-        justifyContent="cen
-      "
+        sx={{ gap: { lg: "62px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
       >
-        {exercises.map((exercise, index) => (
-          <ExerciseCard exercise={exercise} index={index} />
+        {currentExercises.map((exercise, index) => (
+          <ExerciseCard exercise={exercise} index={index} key={exercise.id} />
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisePerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
       </Stack>
     </Box>
   );
